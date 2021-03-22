bf64c3692b09db0aac67903e9ab0a01b6ce3c471
argeus <argeus@debian>
2021-03-22 18:49:10 -0500

    Initial commit.

---
+++ src/mtgblocking/FXMLDocumentController.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mtgblocking;
+
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import static mtgblocking.helpers.runSwipl;
+import static mtgblocking.helpers.writeToFile;
+
+/**
+ *
+ * @author argeus
+ */
+public class FXMLDocumentController implements Initializable {
+    
+    private File dir, path;
+    private String bN, aN, bP, aP, bT, aT;
+    
+    @FXML
+    private TextField plFilePath, blkName, blkPow, blkTou, attName, attPow, attTou;
+    
+    @FXML
+    private void setPlPath(ActionEvent event) {
+        String mtgPlPath = plFilePath.getText();
+        dir = new File(mtgPlPath);
+        path = new File(mtgPlPath + "/mtg.pl");
+    }
+    
+    @FXML
+    private void setPlCreatures(ActionEvent event) {
+        bN = blkName.getText();
+        bP = blkPow.getText();
+        bT = blkTou.getText();
+        
+        aN = attName.getText();
+        aP = attPow.getText();
+        aT = attTou.getText();
+        
+        writeToFile(path, bN, bP, bT, aN, aP, aT);
+        runSwipl(dir);
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
---
+++ src/mtgblocking/helpers.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mtgblocking;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author argeus
+ */
+public class helpers {
+    
+    private static String tradePred = "trade";
+    private static String survivePred = "surviveBlock";
+    
+    private static String writeInit(String pred, String cr1, String cr2){
+        return ":- initialization " + pred + "(" + cr1 + "," + cr2 + ").";    
+    }
+    
+    private static String writeCreature(String crName, String power, String toughness) {
+        return "creature" + "(" + crName + "," + power + "," + toughness + ").";
+    }
+    
+    static void writeToFile(File path, String blkName, String blkPow, String blkTou, String attName, String attPow, String attTou) {
+        try {
+            FileWriter fw = new FileWriter(path, true);
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.newLine();
+            bw.write(writeCreature(blkName, blkPow, blkTou));
+            bw.newLine();
+            bw.write(writeCreature(attName, attPow, attTou));
+            bw.newLine();
+            bw.write(writeInit(tradePred, blkName, attName));
+            bw.newLine();
+            bw.write(writeInit(survivePred, blkName, attName));
+            bw.close();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    
+    static void runSwipl(File dir){
+        try {
+            String sed = "sed -i '30,$d' mtg.pl";
+            String swipl = "swipl -s mtg.pl -g \"saveQueries('1Test.txt'), [mtg], stopQueriesSaving, halt\"";
+            ProcessBuilder pb = new ProcessBuilder("/usr/bin/bash", "-c", swipl);
+            ProcessBuilder pb2 = new ProcessBuilder("/usr/bin/bash", "-c", sed);
+            pb.directory(dir);
+            pb2.directory(dir);
+            pb.start();
+            TimeUnit.MILLISECONDS.sleep(100);
+            pb2.start();
+        } catch (Exception e) {
+            System.out.println("Error " + e.getMessage());
+        }
+    }
+    
+}
